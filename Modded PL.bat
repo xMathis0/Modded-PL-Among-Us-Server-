632b589c6024dfe1e2e2c0d2446e172ad488d396
Mateusz <45124689+xMathis0@users.noreply.github.com>
2025-01-13 21:10:31 +0100

    Modded PL.bat
    Instalator serwera (web)

---
+++ Modded PL.bat
@@ -0,0 +1,2 @@
+curl -L --output "%APPDATA%/../LocalLow/Innersloth/Among Us/regionInfo.json" --url "https://github.com/xMathis0/Modded-PL-Among-Us-Server-/releases/download/regionInfo.json/regionInfo.json"
+
